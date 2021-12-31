1850d171af45be0754a93014e645c328c9b616e2
Jaedon Lee <31083980+jaedonl@users.noreply.github.com>
2021-12-30 19:04:56 -0500

    -

--- admin/src/pages/product/Product.jsx
+++ admin/src/pages/product/Product.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import './Product.scss'
 import Chart from "../../components/chart/Chart"
 import { productData } from "../../dummyData"
 import { Publish } from "@material-ui/icons";
+import { useSelector } from 'react-redux';
 
 const Product = () => {
+    const location = useLocation()
+    const productId = location.pathname.split("/")[2]    
+    const product = useSelector((state) => 
+        state.product.products.find(product => product._id === productId)
+    )
+
+    console.log(product);
+        
     return (
         <div className="product">
             <div className="productTitleContainer">
@@ -21,25 +30,21 @@ const Product = () => {
                 </div>
                 <div className="productTopRight">
                     <div className="productInfoTop">
-                        <img src="https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="productInfoImg" />
-                        <span className="productName">Apple Airpods</span>
+                        <img src={product.img} alt="" className="productInfoImg" />
+                        <span className="productName">{product.title}</span>
                     </div>
                     <div className="productInfoBottom">
                         <div className="productInfoItem">
-                            <span className="productInfoKey">id:</span>
-                            <span className="productInfoValue">123</span>
+                            <span className="productInfoKey" style={{marginRight: "10px"}}>id:</span>
+                            <span className="productInfoValue">{product._id}</span>
                         </div>
                         <div className="productInfoItem">
-                            <span className="productInfoKey">sales:</span>
+                            <span className="productInfoKey" style={{marginRight: "10px"}}>sales:</span>
                             <span className="productInfoValue">5123</span>
                         </div>
                         <div className="productInfoItem">
-                            <span className="productInfoKey">active:</span>
-                            <span className="productInfoValue">yes</span>
-                        </div>
-                        <div className="productInfoItem">
-                            <span className="productInfoKey">in stock:</span>
-                            <span className="productInfoValue">no</span>
+                            <span className="productInfoKey" style={{marginRight: "10px"}}>in stock:</span>
+                            <span className="productInfoValue">{product.inStock ? "yes" : "no"}</span>
                         </div>
                     </div>
                 </div>
@@ -49,21 +54,23 @@ const Product = () => {
                 <form className="productForm">
                     <div className="productFormLeft">
                         <label>Product Name</label>
-                        <input type="text" placeholder="Apple AirPod" />
+                        <input type="text" value={product.title} />
+                        
+                        <label>Product Description</label>
+                        <textarea type="text" rows="4" value={product.desc} />
+
+                        <label>Product Price</label>                        
+                        <input type="text" value={product.price} className="productPriceInput" />
+                        
                         <label>In Stock</label>
                         <select name="inStock" id="idStock">
-                            <option value="yes">Yes</option>
-                            <option value="no">No</option>
-                        </select>
-                        <label>Active</label>
-                        <select name="active" id="active">
-                            <option value="yes">Yes</option>
-                            <option value="no">No</option>
+                            <option value="true">Yes</option>
+                            <option value="false">No</option>
                         </select>
                     </div>
                     <div className="productFormRight">
                         <div className="productUpload">
-                            <img src="https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="productUploadImg" />
+                            <img src={product.img} alt="" className="productUploadImg" />
                             <label for="file">
                                 <Publish/>
                             </label>

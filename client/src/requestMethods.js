9ccc71d84fc32cd224b1b55ff177922c90eca43e
Jaedon Lee <31083980+jaedonl@users.noreply.github.com>
2022-01-10 14:55:38 -0500

    -

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL = "http://localhost:8000/api/"
 // const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.token;
-const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxOWQ2NTcyNDUzZDhiYTBiZTlmMzhiNSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0MTMyOTI0NCwiZXhwIjoxNjQxNTg4NDQ0fQ.SLS-SHjzYl4PyJAik3_Hx7g53jTDC0O8wjqLrJoxjEw'
+const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxOWQ2NTcyNDUzZDhiYTBiZTlmMzhiNSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0MTg0MTMzOSwiZXhwIjoxNjQyMTAwNTM5fQ.o5gFrZa4Nk9DrcNngU1B33dOOJxuIvQLBuVyGkfhSkg'
 
 // const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
 // const currentUser = user && JSON.parse(user).currentUser;
